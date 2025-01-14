a7258c8a3019d845c57a527f7a49d6e8bc3fc767
SaiMidhileshJS <midhileshchowdhary@gmail.com>
2025-01-14 11:45:46 -0500

    Created new Game

---
+++ MadLibs_Game.py
@@ -0,0 +1,10 @@
+# Creating a Mad Libs Story with the inputs from the user
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a Celebrity: ")
+
+print()
+print("Roses are",color)
+print(plural_noun,"are Blue")
+print("I love",celebrity)
