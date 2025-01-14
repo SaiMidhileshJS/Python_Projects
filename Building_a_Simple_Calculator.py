670099d5d33a2450b3cd1fb61cc61b7f7e70c281
SaiMidhileshJS <midhileshchowdhary@gmail.com>
2025-01-14 02:09:43 -0500

    Added few String functions

--- Building_a_Simple_Calculator.py
+++ Building_a_Simple_Calculator.py
@@ -1,1 +1,15 @@
-print("Hello World")
+phrase = "Hello World"
+
+print(phrase.lower())
+
+print(phrase.upper())
+
+print(len(phrase))
+
+print(phrase[0])
+
+print(phrase.index("W"))
+
+print(phrase.index("Hello"))
+
+print(phrase.replace("Hello", "Hey"))
