d3413731a4c3881d384797f5959649d220822e69
SaiMidhileshJS <midhileshchowdhary@gmail.com>
2025-01-15 05:57:10 -0500

    Added other operations

--- Building_a_Simple_Calculator.py
+++ Building_a_Simple_Calculator.py
@@ -5,3 +5,12 @@ num2 = input("Enter another number: ")
 
 result = int(num1) + int(num2)
 print("Addition of", num1,"and", num2, "is", result)
+
+result = int(num1) - int(num2)
+print("Subtraction of", num1,"and", num2, "is", result)
+
+result = int(num1) * int(num2)
+print("Multiplication of", num1,"and", num2, "is", result)
+
+result = int(num1) / int(num2)
+print("Division of", num1,"and", num2, "is", result)
